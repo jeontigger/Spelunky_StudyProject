f189e4b85a4d510c78f65d6bf08f8c9ec56fa761
jeontigger <boy9609@naver.com>
2024-03-18 17:39:27 +0900

    #2 타일블럭 세이브 로드 구현

--- Project/Scripts/CTileBlock.cpp
+++ Project/Scripts/CTileBlock.cpp
@@ -5,13 +5,38 @@
 CTileBlock::CTileBlock()
 	: m_Type(TileBlockType::NONE)
 {
-	for (int i = 0; i < TILEBLOCKSIZE * TILEBLOCKSIZE; i++) {
-		Vec2 pos = math::IdxToRowCol(i, Vec2(TILEBLOCKSIZE, TILEBLOCKSIZE));
-		m_Tiles[(int)pos.x][(int)pos.y] = BlockTileType::Blank;
+	for (int row = 0; row < TILEBLOCKSIZE; row++) {
+		for (int col = 0; col < TILEBLOCKSIZE; col++) {
+			m_Tiles[row][col] = BlockTileType::Blank;
+		}
 	}
-
 }
 
 CTileBlock::~CTileBlock()
 {
+}
+
+void CTileBlock::Save(ofstream& fout)
+{
+	fout << (int)m_Type << endl;
+	for (int row = 0; row < TILEBLOCKSIZE; row++) {
+		for (int col = 0; col < TILEBLOCKSIZE; col++) {
+			fout << (int)m_Tiles[row][col] << " ";
+		}
+		fout << endl;
+	}
+}
+
+void CTileBlock::Load(ifstream& fin)
+{
+	int type;
+	fin >> type;
+	m_Type = (TileBlockType)type;
+
+	for (int row = 0; row < TILEBLOCKSIZE; row++) {
+		for (int col = 0; col < TILEBLOCKSIZE; col++) {
+			fin >> type;
+			m_Tiles[row][col] = (BlockTileType(type));
+		}
+	}
 }
