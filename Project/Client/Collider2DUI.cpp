0f9c7cfd6bf0103cf028a980b156d7ae6dbd800a
jeontigger <boy9609@naver.com>
2024-03-24 17:58:28 +0900

    #6 구현 / Collider2D 컴포넌트 UI 구현

--- Project/Client/Collider2DUI.cpp
+++ Project/Client/Collider2DUI.cpp
@@ -1,9 +1,11 @@
 #include "pch.h"
 #include "Collider2DUI.h"
 
+#include	<Engine/CCollider2D.h>
 
 Collider2DUI::Collider2DUI()
 	: ComponentUI("Collider2D", "##Collider2D", COMPONENT_TYPE::COLLIDER2D)
+	, m_collider(nullptr)
 {
 	SetSize(ImVec2(0.f, 120.f));
 	SetComopnentTitle("Collider2D");
@@ -13,9 +15,28 @@ Collider2DUI::~Collider2DUI()
 {
 }
 
+void Collider2DUI::Activate()
+{
+	ComponentUI::Activate();
+
+	m_collider = (CCollider2D*)GetTargetObject()->GetComponent(COMPONENT_TYPE::COLLIDER2D);
+}
+
 void Collider2DUI::render_update()
 {
 	ComponentUI::render_update();
 
+	Vec2 pos = m_collider->GetOffsetPos();
+	Vec2 scale = m_collider->GetOffsetScale();
+	bool absolute = m_collider->IsAbsolute();
 
+	ImVec2 impos(pos.x, pos.y);
+	
+	ImGui::Text("Offset Pos"); ImGui::SameLine(); ImGui::DragFloat2("##collideroffsetpos", (float*)&pos);
+	ImGui::Text("Offset scale"); ImGui::SameLine(); ImGui::DragFloat2("##collideroffsetscale", (float*)&scale);
+	ImGui::Checkbox("IsAbsolute", &absolute);
+
+	m_collider->SetOffsetPos(pos);
+	m_collider->SetOffsetScale(scale);
+	m_collider->SetAbsolute(absolute);
 }
--- Project/Client/Collider2DUI.h
+++ Project/Client/Collider2DUI.h
@@ -3,7 +3,13 @@
 class Collider2DUI :
     public ComponentUI
 {
+private:
+    class CCollider2D* m_collider;
+
+public:
+    virtual void Activate() override;
     virtual void render_update() override;
+
 public:
     Collider2DUI();
     ~Collider2DUI();
